5ab3c17b373b7e6b3f4b073af1f3a4ced59cab72
XxiHee22 <18602665840@163.com>
2026-04-13 21:55:37 +0800

    Update chinaTimeline.js

--- js/chinaTimeline.js
+++ js/chinaTimeline.js
@@ -339,7 +339,7 @@ export class ChinaTimeline {
       .call(d3.axisLeft(this.yScale)
         .ticks(8, ",.0f")
         .tickValues([1, 2, 5, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000, 100000]
-          .filter(v => v >= this.yScale.domain()[0] && v <= this.yScale.domain()[1])));
+          .filter(v => v >= this.yScale.domain()[0] && v <= this.yScale.domain()[1])))
      .selectAll('text')
       .style('font-size', '12px');
 
